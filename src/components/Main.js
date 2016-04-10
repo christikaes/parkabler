04818807652e1250a3299dacbdbfdcc519583313
Christina <christikaes@gmail.com>
2016-04-10 17:07:22 -0400

    updated babelrc to use root-import

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,17 +1,17 @@
 require('normalize.css/normalize.css');
-require('styles/App.css');
 
 import React from 'react';
 import styles from './Main.scss';
 
-let yeomanImage = require('../images/yeoman.png');
+// let yeomanImage = require('../images/yeoman.png');
+import logo from '~/images/yeoman.png';
 
 class AppComponent extends React.Component {
   render() {
     return (
       <div className="index">
         <div className={styles.test}>TEST</div>
-        <img src={yeomanImage} alt="Yeoman Generator" />
+        <img src={logo} alt="Yeoman Generator" />
         <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
       </div>
     );
