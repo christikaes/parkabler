2ea6a104c203a8387393ba43f0630413bac2ab37
Christina <christikaes@gmail.com>
2016-04-10 15:45:51 -0400

    scss modules

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,6 +2,7 @@ require('normalize.css/normalize.css');
 require('styles/App.css');
 
 import React from 'react';
+import styles from './Main.scss';
 
 let yeomanImage = require('../images/yeoman.png');
 
@@ -9,6 +10,7 @@ class AppComponent extends React.Component {
   render() {
     return (
       <div className="index">
+        <div className={styles.test}>TEST</div>
         <img src={yeomanImage} alt="Yeoman Generator" />
         <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
       </div>
