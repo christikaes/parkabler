c0ca06483a139438671cec4b612a18791b186d86
Christina <christikaes@gmail.com>
2016-04-10 18:39:11 -0400

    [new dist] + added basic map

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,14 +1,31 @@
+
 import React from 'react';
-// import styles from './map.scss';
+import {GoogleMapLoader, GoogleMap, Marker} from "react-google-maps";
 
-class Map extends React.Component {
-  render() {
-    return (
-      <div>
-        MAP
-      </div>
-    );
-  }
+export default function SimpleMap () {
+  return (
+    <section style={{height: "100%"}}>
+      <GoogleMapLoader
+        containerElement={
+          <div
+            style={{
+              height: "300px",
+              width: "300px"
+            }}
+          />
+        }
+        googleMapElement={
+          <GoogleMap
+            ref={(map) => console.log(map)}
+            defaultZoom={3}
+            defaultCenter={{lat: -25.363882, lng: 131.044922}}
+            >
+              <Marker position={{ lat: 25.0112183, lng: 121.52067570000001 }}
+                      key="Taiwan"
+                      defaultAnimation= {2} />
+          </GoogleMap>
+        }
+      />
+    </section>
+  );
 }
-
-export default Map;
