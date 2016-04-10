f4551f27b50739fb1b3e0104887ffb67cf796c2c
Christina <christikaes@gmail.com>
2016-04-10 18:54:06 -0400

    updated map

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,13 +1,94 @@
+import { default as React, Component } from "react";
+import { default as update } from "react-addons-update";
 
-import React from 'react';
-import {GoogleMapLoader, GoogleMap, Marker} from "react-google-maps";
+import { default as canUseDOM } from "can-use-dom";
+import { default as _ } from "lodash";
 
-export default function SimpleMap () {
-  return (
-    <section style={{height: "100%"}}>
+import { GoogleMapLoader, GoogleMap, Marker } from "react-google-maps";
+import { triggerEvent } from "react-google-maps/lib/utils";
+
+/*
+ * This is the modify version of:
+ * https://developers.google.com/maps/documentation/javascript/examples/event-arguments
+ *
+ * Add <script src="https://maps.googleapis.com/maps/api/js"></script> to your HTML to provide google.maps reference
+ */
+export default class GettingStarted extends Component {
+
+
+  constructor(props, context) {
+    super(props, context);
+    this.handleWindowResize = _.throttle(_.bind(this.handleWindowResize, this), 500);
+    this.state = {
+      markers: [{
+        position: {
+          lat: 25.0112183,
+          lng: 121.52067570000001,
+        },
+        key: `Taiwan`,
+        defaultAnimation: 2,
+      }],
+    }
+  }
+
+  componentDidMount() {
+    if (!canUseDOM) {
+      return;
+    }
+    window.addEventListener(`resize`, this.handleWindowResize);
+  }
+
+  componentWillUnmount() {
+    if (!canUseDOM) {
+      return;
+    }
+    window.removeEventListener(`resize`, this.handleWindowResize);
+  }
+
+  handleWindowResize() {
+    console.log(`handleWindowResize`, this._googleMapComponent);
+    triggerEvent(this._googleMapComponent, `resize`);
+  }
+
+  /*
+   * This is called when you click on the map.
+   * Go and try click now.
+   */
+  handleMapClick(event) {
+    let { markers } = this.state;
+    markers = update(markers, {
+      $push: [
+        {
+          position: event.latLng,
+          defaultAnimation: 2,
+          key: Date.now(), // Add a key property for: http://fb.me/react-warning-keys
+        },
+      ],
+    });
+    this.setState({ markers });
+  }
+
+  handleMarkerRightclick(index, event) {
+    /*
+     * All you modify is data, and the view is driven by data.
+     * This is so called data-driven-development. (And yes, it's now in
+     * web front end and even with google maps API.)
+     */
+    let { markers } = this.state;
+    markers = update(markers, {
+      $splice: [
+        [index, 1],
+      ],
+    });
+    this.setState({ markers });
+  }
+
+  render() {
+    return (
       <GoogleMapLoader
         containerElement={
           <div
+            {...this.props}
             style={{
               height: "300px",
               width: "300px"
@@ -16,16 +97,22 @@ export default function SimpleMap () {
         }
         googleMapElement={
           <GoogleMap
-            ref={(map) => console.log(map)}
+            ref={(map) => (this._googleMapComponent = map) && console.log(map)}
             defaultZoom={3}
-            defaultCenter={{lat: -25.363882, lng: 131.044922}}
-            >
-              <Marker position={{ lat: 25.0112183, lng: 121.52067570000001 }}
-                      key="Taiwan"
-                      defaultAnimation= {2} />
+            defaultCenter={{ lat: -25.363882, lng: 131.044922 }}
+            onClick={_.bind(this.handleMapClick, this)}
+          >
+            {this.state.markers.map((marker, index) => {
+              return (
+                <Marker
+                  {...marker}
+                  onRightclick={this.handleMarkerRightclick.bind(this, index)}
+                />
+              );
+            })}
           </GoogleMap>
         }
       />
-    </section>
-  );
+    );
+  }
 }
