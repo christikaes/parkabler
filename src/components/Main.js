2b01cce361b8b3f23fc31cb36dc679ea83446063
Christina <christikaes@gmail.com>
2016-04-10 17:25:24 -0400

    [new dist] + added material-ui

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,19 +1,26 @@
 require('normalize.css/normalize.css');
 
-import React from 'react';
-import styles from './Main.scss';
+// Needed for Material-UI
+import injectTapEventPlugin from 'react-tap-event-plugin';
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
 
-import logo from '~/assets/logo.png';
+import React from 'react';
+import {Paper} from 'material-ui';
+
 import Map from '~/components/Map/Map.js';
 import Splash from '~/components/Splash/Splash.js';
+
+import styles from './Main.scss';
 
 class AppComponent extends React.Component {
   render() {
     return (
-      <div className={styles.app}>
+      <Paper className={styles.app}>
         <Splash />
         <Map />
-      </div>
+      </Paper>
     );
   }
 }
