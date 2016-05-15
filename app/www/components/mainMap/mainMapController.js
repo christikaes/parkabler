8138fbcc057f10ab94b2326da00d2834780aeb62
christikaes <christikaes@gmail.com>
2016-05-15 18:10:53 -0400

    autocomplete search address

--- app/www/components/mainMap/mainMapController.js
+++ app/www/components/mainMap/mainMapController.js
@@ -8,14 +8,27 @@ mainMap.controller('mainMapController', function(NgMap, $firebaseArray){
   this.test = this.markers.length;
 
   var vm = this;
+
+
    vm.dynMarkers = [];
    NgMap.getMap().then(function(map) {
+     vm.map = map;
      for (var i=0; i<vm.markers.length; i++) {
        console.log(vm.markers[i])
        var latLng = new google.maps.LatLng(vm.markers[i].lat, vm.markers[i].lng);
       //  vm.dynMarkers.push(new google.maps.Marker({position:latLng}));
      }
     //  vm.markerClusterer = new MarkerClusterer(map, vm.dynMarkers, {});
+
+
+
+      vm.placeChanged = function() {
+        console.log("PLACE CHANGED")
+        vm.place = this.getPlace();
+        console.log(vm.place)
+        console.log('location', vm.place.geometry.location);
+        vm.map.setCenter(vm.place.geometry.location);
+      }
    });
 });
 
