ccd4121e2713d422091932353c07569b4fe934f4
christikaes <christikaes@gmail.com>
2016-07-31 15:42:23 -0400

    stub for edit spot drawer

--- src/app/editspot/editspot.component.ts
+++ src/app/editspot/editspot.component.ts
@@ -1,9 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { MdUniqueSelectionDispatcher } from '@angular2-material/core';
+import { MdCard } from '@angular2-material/card';
+import { MdButton } from '@angular2-material/button';
+import { MdCheckbox } from '@angular2-material/checkbox';
+import { MdRadioButton, MdRadioGroup } from '@angular2-material/radio';
+import { MdInput } from '@angular2-material/input'
 
 @Component({
   selector: 'edit-spot',
   templateUrl: './editspot.component.html',
-  styleUrls: ['./editspot.component.scss']
+  styleUrls: ['./editspot.component.scss'],
+  providers: [MdUniqueSelectionDispatcher],
+  directives: [ MdCard, MdCheckbox, MdButton, MdRadioGroup, MdRadioButton, MdInput ]
 })
 export class EditSpotComponent implements OnInit {
 
