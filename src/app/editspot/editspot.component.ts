dad8a185c1d9d9cf3b4b0994b0a9c3ccc039a807
rsk7 <ravikrishnasingh@gmail.com>
2016-08-03 00:38:09 -0400

    more stylings

--- src/app/editspot/editspot.component.ts
+++ src/app/editspot/editspot.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgClass } from '@angular/common';
 import { MdUniqueSelectionDispatcher } from '@angular2-material/core';
-import { MdCard } from '@angular2-material/card';
+import { MD_CARD_DIRECTIVES } from '@angular2-material/card';
 import { MdButton } from '@angular2-material/button';
 import { MdCheckbox } from '@angular2-material/checkbox';
 import { MdRadioButton, MdRadioGroup } from '@angular2-material/radio';
@@ -12,7 +12,7 @@ import { MdInput } from '@angular2-material/input';
   templateUrl: './editspot.component.html',
   styleUrls: ['./editspot.component.scss'],
   providers: [MdUniqueSelectionDispatcher], // what's this?
-  directives: [ NgClass, MdCard, MdCheckbox, MdButton, MdRadioGroup, MdRadioButton, MdInput ]
+  directives: [ NgClass, MD_CARD_DIRECTIVES, MdCheckbox, MdButton, MdRadioGroup, MdRadioButton, MdInput ]
 })
 export class EditSpotComponent implements OnInit {
   public enteringSpot: boolean;
