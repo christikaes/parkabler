7ed6a5cf924c67dc4d7cfffcac86cac88709af38
christikaes <christikaes@gmail.com>
2016-09-11 19:59:05 -0400

    Initial Animations

---
+++ src/app/animations.ts
@@ -0,0 +1,24 @@
+import {style, animate, transition, state, trigger} from '@angular/core';
+
+export default [
+  trigger('slideLeft', [
+    state('in', style({transform: 'translateX(0)'})),
+    transition('void => *', [
+      style({transform: 'translateX(100%)'}),
+      animate(400)
+    ]),
+    transition('* => void', [
+      animate(0, style({transform: 'translateX(-100%)'}))
+    ])
+  ]),
+  trigger('slideRight', [
+    state('in', style({transform: 'translateX(0)'})),
+    transition('void => *', [
+      style({transform: 'translateX(-100%)'}),
+      animate(400)
+    ]),
+    transition('* => void', [
+      animate(0, style({transform: 'translateX(100%)'}))
+    ])
+  ])
+];
