3210f27db1776fe296267c61104baa8005ca66b6
christikaes <christikaes@gmail.com>
2016-09-11 21:24:50 -0400

    Update marker icons

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -87,7 +87,21 @@ export class MapComponent implements OnInit, AfterViewInit {
               lng: newMarker.lng
             },
             map: this.map,
-            icon: 'img/marker/marker.png',
+            label: {
+              text: '1', // TODO: This should be newMarker.spots
+              fontSize: '16px',
+              fontWeight: 'bold',
+              color: '#FFF'
+            },
+            icon: {
+                path: 'M0-48c-9.8 0-17.7 7.8-17.7 17.4 0 15.5 17.7 30.6 17.7 30.6s17.7-15.4 17.7-30.6c0-9.6-7.9-17.4-17.7-17.4z',
+                fillColor: '#009688',
+                fillOpacity: 0.9,
+                strokeColor: '#FFF',
+                strokeWeight: 1.5,
+                scale: 1,
+                labelOrigin: new window.google.maps.Point(0, -29)
+            },
             $key: newMarker.$key,
             $lat: newMarker.lat,
             $lng: newMarker.lng
