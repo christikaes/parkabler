b5230f1ae3d438ee57fa16dd4d2fa9f47bb464c5
christikaes <christikaes@gmail.com>
2016-09-11 23:52:11 -0400

    Update publish to publish new tag

--- bin/publish.sh
+++ bin/publish.sh
@@ -48,6 +48,27 @@ git add --all
 git commit -m '[publish] build'
 git push origin gh-pages
 
+echo '_________________________________________'
+echo 'Create and push a new release'
+# Create a new release
+# get highest tag number
+VERSION=`git describe --abbrev=0 --tags`
+
+# replace . with space so can split into an array
+VERSION_BITS=(${VERSION//./ })
+
+# get number parts and increase last one by 1
+VNUM1=${VERSION_BITS[0]}
+VNUM2=${VERSION_BITS[1]}
+VNUM2=$((VNUM2+1))
+
+# create new tag
+NEW_TAG="$VNUM1.$VNUM2"
+
+echo "Updating $VERSION to $NEW_TAG"
+git tag -a $NEW_TAG -m "Update to $NEW_TAG"
+git push --tags
+
 # Restore original state
 git checkout master
 echo '_________________________________________'
