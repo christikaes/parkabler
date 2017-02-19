11712698641cc8b71bff0c45f234a9fba8f20327
christikaes <christikaes@gmail.com>
2017-02-19 14:21:55 -0500

    cleanup mapgl component, create mapbox style

--- src/app/components/map/mapgl/mapgl.component.ts
+++ src/app/components/map/mapgl/mapgl.component.ts
@@ -5,6 +5,7 @@ const mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
 
 // Use this for the opensource maps:
 // var mapstyle = require('./style.json');
+const mapstyle = require('./style_parkabler.json');
 
 @Component({
   selector: 'pa-map-gl',
@@ -25,81 +26,15 @@ export class MapGLComponent extends BaseMapComponent {
     let mapDiv = this.MapDiv.nativeElement;
     let map = new mapboxgl.Map({
       container: mapDiv,
-      style: 'mapbox://styles/mapbox/streets-v9',
+      style: mapstyle,
       center: [-71.06, 42.35],
       zoom: 15
     });
     // HACK: Not sure why canvas is set to absolute position, but it breaks styling:
     map.getCanvas().style.position = 'initial';
 
-    // TODO: a lot of this can be moved to style.json so that it happens before load
-    // Setup the spots layer
+    // Signal that the map is loaded
     map.on('load', () => {
-
-      // Add a new source from our GeoJSON data and set the
-      // 'cluster' option to true.
-      map.addSource('spots', {
-          type: 'geojson',
-          data: {'type': 'FeatureCollection', 'features': []},
-          cluster: true,
-          clusterMaxZoom: 14, // Max zoom to cluster points on
-          clusterRadius: 50 // Radius of each cluster when clustering points (defaults to 50)
-      });
-
-      // Use the spots source to create five layers:
-      // One for unclustered points, three for each cluster category,
-      // and one for cluster labels.
-      map.addLayer({
-          'id': 'unclustered-points',
-          'type': 'symbol',
-          'source': 'spots',
-          'filter': ['!has', 'point_count'],
-          layout: {
-            'icon-image': '{icon}-15',
-            'icon-allow-overlap': true
-          }
-      });
-
-      // Display the spots data in three layers, each filtered to a range of
-      // count values. Each range gets a different fill color.
-      let layers = [
-          [20, '#f28cb1'],
-          [10, '#f1f075'],
-          [0, '#51bbd6']
-      ];
-
-      layers.forEach(function (layer, i) {
-          map.addLayer({
-              'id': 'cluster-' + i,
-              'type': 'circle',
-              'source': 'spots',
-              'paint': {
-                  'circle-color': layer[1],
-                  'circle-radius': 18
-              },
-              'filter': i === 0 ?
-                  ['>=', 'point_count', layer[0]] :
-                  ['all',
-                      ['>=', 'point_count', layer[0]],
-                      ['<', 'point_count', layers[i - 1][0]]]
-          });
-      });
-
-      // Add a layer for the clusters' count labels
-      map.addLayer({
-          'id': 'cluster-count',
-          'type': 'symbol',
-          'source': 'spots',
-          'layout': {
-              'text-field': '{point_count}',
-              'text-font': [
-                  'DIN Offc Pro Medium',
-                  'Arial Unicode MS Bold'
-              ],
-              'text-size': 12
-          }
-      });
-
       done(true);
     });
 
@@ -111,16 +46,15 @@ export class MapGLComponent extends BaseMapComponent {
   }
 
   setMode(mode: MapModes) {
-    // TODO: setting style removes all layers, turn on and off layers instead
     switch (mode) {
       case 'satellite':
-        this.map.setStyle('mapbox://styles/mapbox/satellite-streets-v9');
+        this.map.setPaintProperty('satellite', 'raster-opacity', 1);
         break;
       case 'street':
-        this.map.setStyle('mapbox://styles/mapbox/streets-v9');
+        this.map.setPaintProperty('satellite', 'raster-opacity', 0);
         break;
       default:
-        this.map.setStyle('mapbox://styles/mapbox/streets-v9');
+        this.map.setPaintProperty('satellite', 'raster-opacity', 0);
         break;
     }
   }
