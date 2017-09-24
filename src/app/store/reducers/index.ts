14abbaf7b68045863e059427ac33f11b13978867
christikaes <christikaes@gmail.com>
2017-09-24 11:52:43 -0400

    fixup

--- src/app/store/reducers/index.ts
+++ src/app/store/reducers/index.ts
@@ -6,7 +6,6 @@ export * from './spotsReport.reducer';
 export * from './spotsNearby.reducer';
 export * from './places.reducer';
 export * from './appmode.reducer';
-export * from './spotslistmode.reducer';
 export * from './geolocation.reducer';
 export * from './map.reducer';
 export * from './userID.reducer';
